6d0d3522cb9d3f0def1ea35c9e14ed8e371bc00e
Rolle <roland.schmitt@web.de>
2013-07-26 15:44:43 +0200

    flag

--- app/helpers/maps_helper.rb
+++ app/helpers/maps_helper.rb
@@ -4,7 +4,7 @@ module MapsHelper
 			'<p>'+link_to(image_tag(img.image.url(:thumb)), img) + '</p>' +
 			'<p>'+link_to("Bearbeiten", edit_image_artifact_path(img), :class=>"btn btn-small") + '</p>' +					
 			'<p>'+check_box_tag('ck_main_'+img.id.to_s, img.id, img.main, :data => {:remote=> true, :url => url_for(action: :main, controller: :image_artifacts, id: img.id), id: img.id}) + 'Grube/Stollen' + '</p>' +
-			'<p>'+check_box_tag('ck_adit_'+img.id.to_s, img.id, img.main, :data => {:remote=> true, :url => url_for(action: :adit, controller: :image_artifacts, id: img.id), id: img.id}) + 'Primär' + '</p>' +
+			'<p>'+check_box_tag('ck_adit_'+img.id.to_s, img.id, img.adit, :data => {:remote=> true, :url => url_for(action: :adit, controller: :image_artifacts, id: img.id), id: img.id}) + 'Primär' + '</p>' +
 			'<p>'+check_box_tag('ck_flag_'+img.id.to_s, img.id, img.flag, :data => {:remote=> true, :url => url_for(action: :flag, controller: :image_artifacts, id: img.id), id: img.id}) + 'Mundloch' + '</p>' 
 	end
 end
